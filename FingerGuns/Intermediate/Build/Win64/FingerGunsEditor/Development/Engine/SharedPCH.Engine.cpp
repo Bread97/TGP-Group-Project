c5f0829b367cc5a0aefdd8dbc6c7010a9d318c2d
Test098765432110 <Tracker151@inbox.lv>
2019-01-26 17:58:32 +0000

    Initial commit of the project

---
+++ FingerGuns/Intermediate/Build/Win64/FingerGunsEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/Xyvn/Documents/Unreal Projects/FingerGuns/Intermediate/Build/Win64/FingerGunsEditor/Development/Engine/SharedPCH.Engine.h"
---
+++ FingerGuns/Intermediate/Build/Win64/UE4Editor/Inc/FingerGuns/FingerGunsGameModeBase.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef FINGERGUNS_FingerGunsGameModeBase_generated_h
+#error "FingerGunsGameModeBase.generated.h already included, missing '#pragma once' in FingerGunsGameModeBase.h"
+#endif
+#define FINGERGUNS_FingerGunsGameModeBase_generated_h
+
+#define FingerGuns_Source_FingerGuns_FingerGunsGameModeBase_h_15_RPC_WRAPPERS
+#define FingerGuns_Source_FingerGuns_FingerGunsGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define FingerGuns_Source_FingerGuns_FingerGunsGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAFingerGunsGameModeBase(); \
+	friend struct Z_Construct_UClass_AFingerGunsGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AFingerGunsGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/FingerGuns"), NO_API) \
+	DECLARE_SERIALIZER(AFingerGunsGameModeBase)
+
+
+#define FingerGuns_Source_FingerGuns_FingerGunsGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAFingerGunsGameModeBase(); \
+	friend struct Z_Construct_UClass_AFingerGunsGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AFingerGunsGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/FingerGuns"), NO_API) \
+	DECLARE_SERIALIZER(AFingerGunsGameModeBase)
+
+
+#define FingerGuns_Source_FingerGuns_FingerGunsGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AFingerGunsGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AFingerGunsGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AFingerGunsGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AFingerGunsGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AFingerGunsGameModeBase(AFingerGunsGameModeBase&&); \
+	NO_API AFingerGunsGameModeBase(const AFingerGunsGameModeBase&); \
+public:
+
+
+#define FingerGuns_Source_FingerGuns_FingerGunsGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AFingerGunsGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AFingerGunsGameModeBase(AFingerGunsGameModeBase&&); \
+	NO_API AFingerGunsGameModeBase(const AFingerGunsGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AFingerGunsGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AFingerGunsGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AFingerGunsGameModeBase)
+
+
+#define FingerGuns_Source_FingerGuns_FingerGunsGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define FingerGuns_Source_FingerGuns_FingerGunsGameModeBase_h_12_PROLOG
+#define FingerGuns_Source_FingerGuns_FingerGunsGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FingerGuns_Source_FingerGuns_FingerGunsGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	FingerGuns_Source_FingerGuns_FingerGunsGameModeBase_h_15_RPC_WRAPPERS \
+	FingerGuns_Source_FingerGuns_FingerGunsGameModeBase_h_15_INCLASS \
+	FingerGuns_Source_FingerGuns_FingerGunsGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FingerGuns_Source_FingerGuns_FingerGunsGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FingerGuns_Source_FingerGuns_FingerGunsGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	FingerGuns_Source_FingerGuns_FingerGunsGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	FingerGuns_Source_FingerGuns_FingerGunsGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	FingerGuns_Source_FingerGuns_FingerGunsGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FingerGuns_Source_FingerGuns_FingerGunsGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
