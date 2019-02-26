8f0fb87d43369cf9b01077fed82a14e1687a79a0
LO177 <l017081h@student.staffs.ac.uk>
2019-02-26 18:12:28 +0000

    New map with some new assets, blueprints + meshes of the new assets as well as Maya and FBX files for the models. Models and Blueprints folders added as well.

--- FingerGunz/Intermediate/Build/Win64/FingerGunzEditor/Development/Engine/SharedPCH.Engine.cpp
+++ FingerGunz/Intermediate/Build/Win64/FingerGunzEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,1 +1,1 @@
-#include "C:/Users/w019563h.STUDENT/Documents/TGP-Group-Project/FingerGunz/Intermediate/Build/Win64/FingerGunzEditor/Development/Engine/SharedPCH.Engine.h"
+#include "D:/Y2/SourceTreeRepositories/FingerGunz/Intermediate/Build/Win64/FingerGunzEditor/Development/Engine/SharedPCH.Engine.h"
